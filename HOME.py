a7347c31770e1e30cbcdc6c7ba0a3f50752bf3da
MK <99416359+MK316@users.noreply.github.com>
2026-01-11 22:51:04 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -4,18 +4,20 @@ st.markdown("### 🐾 Streamlit into your class")
 st.caption("2026. 01. 12. Workshop")
 
 
-col_l, col_c, col_r = st.columns([0.5, 2, 0.5])
+st.image(
+    "https://raw.githubusercontent.com/MK316/gnu260112/main/images/bg01.png",
+    width=100,
+    caption="Teaching is one of the best ways to learn."
+)
 
-with col_c:
-    st.markdown(
-        """
-        <div style="text-align: center;">
-            <img src="https://raw.githubusercontent.com/MK316/gnu260112/main/images/bg01.png" width="300"><br>
-            <em>Teaching is one of the best ways to learn.</em><br><br>
-            <img src="https://raw.githubusercontent.com/MK316/gnu260112/main/images/qr01.png" width="100"><br>
-            <em>Access QR</em>
-        </div>
-        """,
-        unsafe_allow_html=True
+st.image(
+    "https://raw.githubusercontent.com/MK316/gnu260112/main/images/qr01.png",
+    width=100,
+    caption="QR code (click below to enlarge)"
+)
+
+with st.expander("🔍 View full size"):
+    st.image(
+        "https://raw.githubusercontent.com/MK316/gnu260112/main/images/qr01.png",
+        use_container_width=True
     )
-
