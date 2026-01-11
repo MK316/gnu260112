8b10b293c57d7e8750848d2ccbf8d3044fc1b687
MK <99416359+MK316@users.noreply.github.com>
2026-01-11 23:25:47 +0900

    Update 3🌱_Streamlit_samples.py

--- pages/3🌱_Streamlit_samples.py
+++ pages/3🌱_Streamlit_samples.py
@@ -1,3 +1,33 @@
 import streamlit as st
 
-st.caption("MK316 digital classroom")
+st.set_page_config(page_title="Streamlit samples", layout="wide")
+
+st.markdown("## 🔗 Streamlit app samples")
+st.caption("course LMS, app development, workshop platform, collaboration, etc.")
+
+st.markdown("---")
+
+# -------- Item 1 --------
+st.markdown("### 🍰 MK316 Homepage")
+st.markdown("🔗 https://github.com")
+st.caption(
+    "개인 홈페이지 가능 (Github 계정) "
+)
+
+st.markdown("---")
+
+# -------- Item 2 --------
+st.markdown("### 🍨 Digital Classroom")
+st.markdown("🔗 https://mk316home.streamlit.app")
+st.caption(
+    "MK316 Digital classroom archives"
+)
+
+st.markdown("---")
+
+# -------- Item 3 --------
+st.markdown("### 🍮 Workshops")
+st.markdown("🔗 https://github.com/MK316/workshops/blob/main/README.md")
+st.caption(
+    "Workshop presentation, file repository, collaboration, etc. "
+)
