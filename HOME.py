1c35a472a51b79ae7a05d08fc0fc40b08ae0f326
MK <99416359+MK316@users.noreply.github.com>
2026-01-11 22:38:27 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -8,7 +8,7 @@ st.caption(
 
 col_l, col_c, col_r = st.columns([1,2,1])
 with col_c:
-    st.image("https://github.com/MK316/gnu20260112/raw/main/images/bg01.png",
+    st.image("https://raw.githubusercontent.com/MK316/gnu260112/main/images/bg01.png",
              caption="Teaching is one of the best ways to learn.", width=300)
-    st.image("https://github.com/MK316/gnu20260112/raw/main/images/qr01.png",
+    st.image("https://raw.githubusercontent.com/MK316/gnu260112/main/images/qr01.png",
              caption="Access QR", width=100)
