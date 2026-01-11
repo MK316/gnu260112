1497a35d8eb6955e5cd229d5a219cf61c23d4fdb
MK <99416359+MK316@users.noreply.github.com>
2026-01-11 22:32:04 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
-st.caption("Streamlit into your class; GNU Humanities")
-st.caption("2026.01.12")
+st.markdown("### Streamlit into your class")
+st.caption(
+    "2026. 01. 12. Workshop"
+)
+
+
+col_l, col_c, col_r = st.columns([1,2,1])
+with col_c:
+    st.image("https://github.com/MK316/gnu20260112/raw/main/images/bg01.png",
+             caption="Teaching is one of the best ways to learn.", width=300)
+    st.image("https://github.com/MK316/gnu20260112/raw/main/images/qr01.png",
+             caption="Access QR", width=100)
